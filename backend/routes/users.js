ee52d92c4caa7fe3429ee49502b4a21edacd881f
Satoshi Matsushima <>
2022-06-18 15:52:22 +0900

    modified users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -52,7 +52,7 @@ router.delete('/:id', async (req, res) => {
 // })
 
 // クエリでユーザー情報を取得
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
   const userId = req.query.userId
   const username = req.query.username
 
